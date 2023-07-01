84e61e376dbaa5763a8618a5dc8a5375da062e78
Henrik <henrik.enquist@gmail.com>
2023-07-01 12:32:46 +0200

    Use audio iterators for smarter slice copy

--- src/audio.rs
+++ src/audio.rs
@@ -30,7 +30,19 @@ where
         self.get(channel).unwrap().get(frame).unwrap()
     }
 
-    // TODO smarter slice copy
+    fn write_from_channel_to_slice(&self, channel: usize, skip: usize, slice: &mut [T]) -> usize {
+        if channel >= self.channels() || skip >= self.frames() {
+            return 0;
+        }
+        let frames_to_write = if (self.frames() - skip) < slice.len() {
+            self.frames() - skip
+        } else {
+            slice.len()
+        };
+        let chan = self.get(channel).unwrap();
+        chan.iter().skip(skip).take(frames_to_write).zip(slice.iter_mut()).for_each(|(s, o)| *o = s);
+        frames_to_write
+    }
 }
 
 impl<'a, T, U> IndirectMut<'a, T> for U
@@ -44,7 +56,24 @@ where
         false
     }
 
-    // TODO smarter slice copy
+    fn write_from_slice_to_channel(
+        &mut self,
+        channel: usize,
+        skip: usize,
+        slice: &[T],
+    ) -> (usize, usize) {
+        if channel >= Indirect::channels(self) || skip >= Indirect::frames(self) {
+            return (0, 0);
+        }
+        let frames_to_read = if (Indirect::frames(self) - skip) < slice.len() {
+            Indirect::frames(self) - skip
+        } else {
+            slice.len()
+        };
+        let mut chan = self.get_mut(channel).unwrap();
+        chan.iter_mut().skip(skip).take(frames_to_read).zip(slice.iter()).for_each(|(s, o)| *s = *o);
+        (frames_to_read, 0)
+    }
 }
 
 // TODO implement Direct and DirectMut
@@ -83,6 +112,27 @@ mod tests {
         assert_eq!(buf.get(1).unwrap().get(0).unwrap(), 2);
         assert_eq!(buf.get(0).unwrap().get(1).unwrap(), 3);
         assert_eq!(buf.get(1).unwrap().get(1).unwrap(), 4);
+    }
+
+    #[test]
+    fn read_to_slice() {
+        let mut other = vec![0; 3];
+        let buf = wrap::interleaved(&[1, 2, 3, 4, 5, 6, 7, 8], 2);
+        buf.write_from_channel_to_slice(0, 1, &mut other);
+        assert_eq!(other[0], 3);
+        assert_eq!(other[1], 5);
+        assert_eq!(other[2], 7);
+    }
+
+    #[test]
+    fn write_to_slice() {
+        let other = vec![1, 2, 3];
+        let mut buf = audio::buf::Interleaved::<i32>::with_topology(2, 4);
+        buf.write_from_slice_to_channel(0, 1, &other);
+        assert_eq!(buf.get(0).unwrap().get(0).unwrap(), 0);
+        assert_eq!(buf.get(0).unwrap().get(1).unwrap(), 1);
+        assert_eq!(buf.get(0).unwrap().get(2).unwrap(), 2);
+        assert_eq!(buf.get(0).unwrap().get(3).unwrap(), 3);
     }
 }
 
