1dc1c58fd5c09ea34d01467ab26506eebc87a68c
HEnquist <henrik.enquist@gmail.com>
2025-03-17 09:03:21 +0100

    Add trait methods for reading and writing raw samples

--- src/readwrite.rs
+++ src/readwrite.rs
@@ -4,6 +4,31 @@ use std::io;
 
 /// A trait that extends [std::io::Read] with methods for reading samples directly.
 pub trait ReadSamples: io::Read {
+    /// Read a single sample from the underlying reader.
+    ///
+    /// This method reads a chunk of bytes from the underlying reader,
+    /// and interprets it as a sample of type `T`.
+    ///
+    /// # Type Parameters
+    ///
+    /// * `T`: A type implementing the `BytesSample` trait, defining the format of the sample to read.
+    ///
+    /// # Returns
+    ///
+    /// * `io::Result<T>`: The read sample, or an error if reading failed.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if:
+    ///
+    /// * The underlying reader returns an error.
+    /// * The number of bytes read is not sufficient to represent a complete sample.
+    fn read_sample<T: BytesSample>(&mut self) -> io::Result<T> {
+        let mut sample: T = unsafe { std::mem::zeroed() };
+        self.read_exact(sample.as_mut_slice())?;
+        Ok(sample)
+    }
+
     /// Read a single sample and return it as a numeric type.
     ///
     /// This method reads a chunk of bytes from the underlying reader,
@@ -25,9 +50,8 @@ pub trait ReadSamples: io::Read {
     /// * The underlying reader returns an error.
     /// * The number of bytes read is not sufficient to represent a complete sample.
     fn read_number<T: BytesSample>(&mut self) -> io::Result<T::NumericType> {
-        let mut number: T = unsafe { std::mem::zeroed() };
-        self.read_exact(number.as_mut_slice())?;
-        Ok(number.to_number())
+        let sample = self.read_sample::<T>()?;
+        Ok(sample.to_number())
     }
 
     /// Read a single sample and convert it to a floating-point number.
@@ -54,9 +78,36 @@ pub trait ReadSamples: io::Read {
     /// * The underlying reader returns an error.
     /// * The number of bytes read is not sufficient to represent a complete sample.
     fn read_converted<T: RawSample + BytesSample, U: Float>(&mut self) -> io::Result<U> {
-        let mut sample: T = unsafe { std::mem::zeroed() };
-        self.read_exact(sample.as_mut_slice())?;
+        let sample = self.read_sample::<T>()?;
         Ok(sample.to_scaled_float::<U>())
+    }
+
+    /// Read multiple samples into a slice.
+    ///
+    /// This method attempts to read enough bytes from the underlying reader to
+    /// fill the buffer `buf` with samples of type `T`. It reads samples one at a time,
+    /// populating the buffer in order.
+    ///
+    /// # Type Parameters
+    ///
+    /// * `T`: A type implementing the `BytesSample` trait, defining the format of the samples to read.
+    ///
+    /// # Arguments
+    ///
+    /// * `buf`: A mutable slice where the samples will be stored.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if:
+    ///
+    /// * The underlying reader returns an error.
+    /// * The number of bytes read is not sufficient to represent a complete sample.
+    /// * The end of the reader is reached before all samples have been read.
+    fn read_samples_exact<T: BytesSample>(&mut self, buf: &mut [T]) -> io::Result<()> {
+        for sample in buf.iter_mut() {
+            *sample = self.read_sample()?;
+        }
+        Ok(())
     }
 
     /// Read multiple samples and store them as numeric types in a provided buffer.
@@ -126,6 +177,36 @@ pub trait ReadSamples: io::Read {
             *sample = self.read_converted::<T, U>()?;
         }
         Ok(())
+    }
+
+    /// Read samples until the end of the stream, storing them in a vector.
+    ///
+    /// This method reads samples from the underlying reader until an error is encountered
+    /// (typically, the end of the stream). Each sample is read and interpreted as a type `T`
+    /// before being appended to the provided vector `buf`.
+    ///
+    /// # Type Parameters
+    ///
+    /// * `T`: A type implementing the `BytesSample` trait, defining the format of the samples to read.
+    ///
+    /// # Arguments
+    ///
+    /// * `buf`: A mutable vector where the samples will be appended.
+    ///
+    /// # Returns
+    /// The number of samples read.
+    ///
+    /// # Errors
+    ///
+    /// * The underlying reader returns an error (except for EOF).
+    /// * The number of bytes read is not sufficient to represent a complete sample.
+    fn read_samples_to_end<T: BytesSample>(&mut self, buf: &mut Vec<T>) -> io::Result<usize> {
+        let mut count = 0;
+        while let Ok(sample) = self.read_sample::<T>() {
+            buf.push(sample);
+            count += 1;
+        }
+        Ok(count)
     }
 
     /// Read samples until the end of the stream, storing them as numeric types in a vector.
@@ -207,6 +288,27 @@ impl<R: io::Read + ?Sized> ReadSamples for R {}
 
 /// A trait that extends [std::io::Write] with methods for writing samples directly.
 pub trait WriteSamples: io::Write {
+    /// Write a single sample to the underlying writer.
+    ///
+    /// This method takes a reference to a sample of type `T`,
+    /// gets its raw byte representation using the `as_slice` method,
+    /// and writes it to the underlying writer.
+    ///
+    /// # Type Parameters
+    ///
+    /// * `T`: A type implementing the `BytesSample` trait, defining the format of the sample to write.
+    /// # Returns
+    ///
+    /// * `io::Result<()>`: Ok(()) if the sample was written successfully.
+    /// # Errors
+    ///
+    /// This function will return an error if:
+    ///
+    /// * The underlying writer returns an error.
+    fn write_sample<T: BytesSample>(&mut self, sample: &T) -> io::Result<()> {
+        self.write_all(sample.as_slice())
+    }
+
     /// Write a single sample from a numeric type to the underlying writer.
     ///
     /// This method takes a sample represented by its `NumericType`,
@@ -264,6 +366,32 @@ pub trait WriteSamples: io::Write {
         let converted = T::from_scaled_float(value);
         self.write_all(converted.value.as_slice())?;
         Ok(converted.clipped)
+    }
+
+    /// Write multiple samples to the underlying writer.
+    ///
+    /// This method takes a slice of samples, gets the raw byte representation of each sample,
+    /// and then writes it to the underlying writer.
+    ///
+    /// # Type Parameters
+    ///
+    /// * `T`: A type implementing the `BytesSample` trait, defining the format of the samples to write.
+    /// # Arguments
+    ///
+    /// * `samples`: The samples to write.
+    /// # Returns
+    ///
+    /// * `io::Result<()>`: Ok(()) if all samples were written successfully.
+    /// # Errors
+    ///
+    /// This function will return an error if:
+    ///
+    /// * The underlying writer returns an error.
+    fn write_all_samples<T: BytesSample>(&mut self, samples: &[T]) -> io::Result<()> {
+        for sample in samples {
+            self.write_sample::<T>(sample)?;
+        }
+        Ok(())
     }
 
     /// Write multiple samples from a numeric slice to the underlying writer.
