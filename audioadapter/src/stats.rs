62150f62ba0f5e747ff49db49d9824e2db4348a6
Henrik <henrik.enquist@gmail.com>
2026-03-31 00:20:08 +0200

    Typo

--- audioadapter/src/stats.rs
+++ audioadapter/src/stats.rs
@@ -2,7 +2,7 @@ use num_traits::{Num, ToPrimitive};
 
 use crate::Adapter;
 
-/// A simple implementation of the Newton's method for calculating the square root of a number.
+/// A simple implementation of Newton's method for calculating the square root of a number.
 /// This is used to avoid depending on `std`, until math support in core is stable.
 /// See: <https://doc.rust-lang.org/core/f64/math/fn.sqrt.html>
 ///
